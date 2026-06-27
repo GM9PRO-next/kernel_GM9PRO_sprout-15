141ed8661b6bd306dfeca4d49a82380ad6a61412
yigityanik <yeyanik123@gmail.com>
2026-06-27 23:10:19 +0300

    Import GM9 Pro sec_ts touchscreen driver

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -758,6 +758,7 @@
 #define SW_ROTATE_LOCK		0x0c  /* set = rotate locked/disabled */
 #define SW_LINEIN_INSERT	0x0d  /* set = inserted */
 #define SW_MUTE_DEVICE		0x0e  /* set = device disabled */
+#define SW_GLOVE		0x0f	/* set = glove mode */
 #define SW_HPHL_OVERCURRENT	0x0f  /* set = over current on left hph */
 #define SW_HPHR_OVERCURRENT	0x10  /* set = over current on right hph */
 #define SW_MICROPHONE2_INSERT   0x11  /* set = inserted */
